d7b0628c58fea9c231c9df73ba3d7077f41f307a
deepak-chaurasiya12 <dchaurasiya277@gmail.com>
2024-05-19 16:02:30 +0530

    made chnages in form.js of frotend code

--- Client/src/Components/Form.js
+++ Client/src/Components/Form.js
@@ -2,11 +2,6 @@ import React, { useState } from 'react';
 import './Asset/Form.css';
 import axios from 'axios';
 
-<<<<<<< HEAD
-=======
-axios.defaults.baseURL = 'https://my-poortfolio-seven.vercel.app/'
-
->>>>>>> 6027039bc308e8ba17e51363cca62c77c1000059
 const Form = () => {
   const [formData, setFormData] = useState({
     name: '',
@@ -24,14 +19,11 @@ const Form = () => {
   };
 
   const handleSubmit = async (e) => {
-<<<<<<< HEAD
     e.preventDefault();
-    
+
     try {
-      // Send form data to backend
+      // Use your deployed backend URL
       await axios.post('https://my-poortfolio-livid.vercel.app/api/saveFormData', formData);
-; // changed the URL to relative path
-      // Optionally, you can clear the form fields after saving
       setFormData({
         name: '',
         email: '',
@@ -41,30 +33,10 @@ const Form = () => {
       alert('Form data submitted successfully');
     } catch (error) {
       console.error('Error saving form data:', error);
-      console.log('Error response:', error.response); // Log the error response
+      console.log('Error response:', error.response);
       alert('Error saving form data. Please try again later.');
     }
   };
-=======
-  e.preventDefault();
-  
-  try {
-    // Use your deployed backend URL
-    await axios.post('https://my-poortfolio-seven.vercel.app//api/saveFormData', formData);
-    setFormData({
-      name: '',
-      email: '',
-      subject: '',
-      message: ''
-    });
-    alert('Form data submitted successfully');
-  } catch (error) {
-    console.error('Error saving form data:', error);
-    console.log('Error response:', error.response);
-    alert('Error saving form data. Please try again later.');
-  }
-};
->>>>>>> 6027039bc308e8ba17e51363cca62c77c1000059
 
   return (
     <div className='form'>
