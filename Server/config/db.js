f81dbcb3044e81bc57a410784c30604a5778b327
deepak-chaurasiya12 <dchaurasiya277@gmail.com>
2024-05-22 17:35:46 +0530

    remove some funtions from db

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,16 +1,16 @@
-// config/db.js
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+  if (mongoose.connection.readyState >= 1) {
+    return; // Connection is already established
+  }
+
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log('MongoDB connected');
   } catch (err) {
-    console.error('Error connecting to MongoDB:', err.message);
-    process.exit(1);
+    console.error('Error connecting to MongoDB:', err);
+    throw err; // Re-throw the error to be caught in the index.js
   }
 };
 
