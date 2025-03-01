2afcffe208f98f43220d51146246d704e87e878b
deepak-chaurasiya12 <dchaurasiya277@gmail.com>
2025-03-01 19:22:23 +0530

    made many changes

--- Client/src/Components/AboutContent.js
+++ Client/src/Components/AboutContent.js
@@ -1,32 +1,30 @@
-import React from 'react'
-import './Asset/AboutContent.css'
-import { Link } from 'react-router-dom'
-import pro1 from '../Components/Asset/Images/ia.webp'
+import React from 'react';
+import './Asset/AboutContent.css';
+import { Link } from 'react-router-dom';
+import pro1 from '../Components/Asset/Images/pic.webp';
+
 const AboutContent = () => {
   return (
     <div className='about'>
-        <div className='left'>
-            <h1>Who am I?</h1>
-            <p>I am a React web developer specializing in creating responsive, secure, and high-performance websites tailored to my clients' needs. I leverage AI-driven design techniques to enhance user experience and make web pages more visually appealing. With a focus on modern aesthetics, functionality, and security, I ensure that every project meets the highest industry standards.</p>
-            <Link to="/contact">
-                <button className='btn'>Contact</button>
-            </Link>
-
+      <div className='left'>
+        <h1>Who am I?</h1>
+        <p>I am a React web developer specializing in creating responsive, secure, and high-performance websites tailored to my clients' needs. I leverage AI-driven design techniques to enhance user experience and make web pages more visually appealing. With a focus on modern aesthetics, functionality, and security, I ensure that every project meets the highest industry standards.</p>
+        <Link to="/contact">
+          <button className='btn'>Contact</button>
+        </Link>
+      </div>
+      <div className='right'>
+        <div className='img-container'>
+          <div className='img-stack top rotating'>
+            <img src={pro1} className='img' alt='profile' />
+          </div>
+          <div className='img-stack bottom rotating'>
+            <img src={pro1} className='img' alt='profile' />
+          </div>
         </div>
-
-        <div className='right'>
-            <div className='img-container'>
-                <div className='img-stack top'>
-                   <img src={pro1} className='img' alt='true'/> 
-                </div>
-                <div className='img-stack bottom'>
-                   <img src={pro1} className='img' alt='pic1'/> 
-                </div>
-                
-            </div>
-        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutContent
+export default AboutContent;
