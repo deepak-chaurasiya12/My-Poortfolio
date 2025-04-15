5dcca935edcdbaf4dcfc284a9885da51058b0289
deepak-chaurasiya12 <dchaurasiya277@gmail.com>
2025-04-15 16:39:55 +0530

    Made changes in Heroimg.js file

--- Client/src/Components/Heroimg.js
+++ Client/src/Components/Heroimg.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import IntroImg1 from "../Components/Asset/Images/ia.webp";
+import ProfilePic from "../Components/Asset/Images/profile.jpg"; // ← your profile image
 import { Link } from 'react-router-dom';
 import './Asset/Heroimg.css';
 
@@ -11,12 +12,14 @@ const Heroimg = () => {
         </div>
 
         <div className='content'>
+          {/* Profile picture */}
+          <img src={ProfilePic} alt="Deepak Profile" className="profile-img" />
             <p>Hi, I am Deepak</p>
+
             <div>
                 <Link to="/project" className='btn'>Projects</Link>
                 <Link to='/contact' className='btn btn-light'>Contact</Link>
 
-                {/* Open CV in a new tab */}
                 <a href="/Deepak_CV.pdf" target="_blank" rel="noopener noreferrer" className='btn btn-cv'>
                   View CV
                 </a>
